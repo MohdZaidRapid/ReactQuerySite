ae6e750b6106e087e106b89c891d858554e1bf4f
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2024-01-11 15:27:22 +0530

    initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,19 +7,31 @@ import axios from "axios";
 function App() {
   const [products, setProducts] = useState([]);
   const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
     // const response=  await axios.get();
     (async () => {
       try {
+        setLoading(true);
         setError(false);
         const response = await axios.get("/api/products");
         console.log(response.data);
         setProducts(response.data);
+        setLoading(false);
       } catch (error) {
         setError(true);
+        setLoading(false);
       }
     })();
   }, []);
+
+  if (error) {
+    return <h1>Something went wrong</h1>;
+  }
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
 
   return (
     <>
