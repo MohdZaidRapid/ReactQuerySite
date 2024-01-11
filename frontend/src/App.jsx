a94d92d135fc7e2dddb615c4d34bc4b589609159
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2024-01-11 16:22:55 +0530

    initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,61 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import axios from "axios";
 
 function App() {
-  const [products, error, loading] = customReactQuery("/api/products");
-  if (error) {
-    return <h1>Something went wrong</h1>;
-  }
+  // const [products, error, loading] = customReactQuery("/api/products");
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [search, setSearch] = useState("");
+  useEffect(() => {
+    const controller = new AbortController();
+    // const response=  await axios.get();
+    (async () => {
+      try {
+        setLoading(true);
+        setError(false);
+        const response = await axios.get("/api/products?search=" + search, {
+          signal: controller.signal,
+        });
+        console.log(response.data);
+        setProducts(response.data);
+        setLoading(false);
+      } catch (error) {
+        if (axios.isCancel(error)) {
+          console.log("Request canceled", error.message);
+          return;
+        }
+        setError(true);
+        setLoading(false);
+      }
+    })();
+    // clean up
+    return () => {
+      controller.abort();
+    };
+  }, [search]);
 
-  if (loading) {
-    return <h1>Loading...</h1>;
-  }
+  // if (error) {
+  //   return <h1>Something went wrong</h1>;
+  // }
+
+  // if (loading) {
+  //   return <h1>Loading...</h1>;
+  // }
 
   return (
     <>
+      <input
+        type="text"
+        placeholder="Search"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+
       <h1>React Query</h1>
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>Somehting went wrong</h1>}
       <h2>Number of products are:{products.length}</h2>
     </>
   );
@@ -24,25 +63,25 @@ function App() {
 
 export default App;
 
-const customReactQuery = (urlPath) => {
-  const [products, setProducts] = useState([]);
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false);
-  useEffect(() => {
-    // const response=  await axios.get();
-    (async () => {
-      try {
-        setLoading(true);
-        setError(false);
-        const response = await axios.get(urlPath);
-        console.log(response.data);
-        setProducts(response.data);
-        setLoading(false);
-      } catch (error) {
-        setError(true);
-        setLoading(false);
-      }
-    })();
-  }, []);
-  return [products, error, loading];
-};
+// const customReactQuery = (urlPath) => {
+//   const [products, setProducts] = useState([]);
+//   const [error, setError] = useState(false);
+//   const [loading, setLoading] = useState(false);
+//   useEffect(() => {
+//     // const response=  await axios.get();
+//     (async () => {
+//       try {
+//         setLoading(true);
+//         setError(false);
+//         const response = await axios.get(urlPath);
+//         console.log(response.data);
+//         setProducts(response.data);
+//         setLoading(false);
+//       } catch (error) {
+//         setError(true);
+//         setLoading(false);
+//       }
+//     })();
+//   }, []);
+//   return [products, error, loading];
+// };
