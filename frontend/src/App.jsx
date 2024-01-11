f4505e0efc4644cadb7f09f3ab38e0d4c5946b36
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2024-01-11 15:22:20 +0530

    initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(false);
+  useEffect(() => {
+    // const response=  await axios.get();
+    (async () => {
+      try {
+        setError(false);
+        const response = await axios.get("/api/products");
+        console.log(response.data);
+        setProducts(response.data);
+      } catch (error) {
+        setError(true);
+      }
+    })();
+  }, []);
 
   return (
     <>
-      <h1>React router dom</h1>
+      <h1>React Query</h1>
+      <h2>Number of products are:{products.length}</h2>
     </>
   );
 }
