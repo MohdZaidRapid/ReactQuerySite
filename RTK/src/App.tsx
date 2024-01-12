44b35fb33f62cd3fc53ad4da0ff67628e3c83dd2
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2024-01-13 02:29:03 +0530

    added

--- RTK/src/App.tsx
+++ RTK/src/App.tsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import React, { FormEvent, useState } from 'react'
+import { useGetPostsQuery, useNewPostMutation } from './redux/api'
+import PostCard from './components/PostCard'
 
 const App = () => {
+  const {isLoading,isError,isSuccess,data,error}=useGetPostsQuery("")
+  // console.log(isLoading,isError,isSuccess,data,error)
+  const [newPost]=useNewPostMutation()
+
+  const [title,setTitle]=useState<string>("")
+  const [body,setBody]=useState<string>("")
+
+const submitHandler=(e:FormEvent<HTMLFormElement>):void=>{
+e.preventDefault();
+
+const post:Post={
+  title,
+  body,
+  userId:Math.random()*1000,
+  id:Math.random()*1000,
+
+}
+newPost(post)
+setTitle("")
+setBody("")
+} 
+  
   return (
-    <div>App</div>
+    <div>
+      <h1>My App</h1>
+<form onSubmit={submitHandler}>
+      <input type="text" placeholder='Title' value={title}
+      onChange={(e)=>setTitle(e.target.value)} />
+
+      <input type="text" placeholder='Body' value={body} onChange={(e)=>setBody(e.target.value)} />
+      <button type='submit'>Add</button>
+      </form>
+    {
+      isLoading?<div>Loading........</div>:
+      data?.map(i=>(
+        <PostCard key={i.id} post={i} />
+      ))
+    }
+    </div>
   )
 }
 
--- RTK/src/redux/api.ts
+++ RTK/src/redux/api.ts
@@ -1,11 +1,26 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const posts = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
     baseUrl: "http://localhost:3000/",
   }),
+  tagTypes: ["Posts"],
   endpoints: (builder) => ({
-    getPosts: builder.query<string, string>({ query: () => "posts" }),
+    getPosts: builder.query<Post[], string>({
+      query: () => "posts",
+      providesTags: ["Posts"],
+    }),
+
+    newPost: builder.mutation<Post, Post>({
+      query: (post) => ({
+        url: "posts",
+        method: "POST",
+        body: post,
+      }),
+      invalidatesTags: ["Posts"],
+    }),
   }),
 });
+
+export const { useGetPostsQuery, useNewPostMutation } = posts;
